5a14b855588645456faf74d57ae61bb5d0479435
james inch <james.inch0103@gmail.com>
2018-08-07 22:17:57 -0400

    add html wireframe and firebase(config)

---
+++ assets/apps.js
@@ -0,0 +1,20 @@
+// When adding trains, administrators should be able to submit the following:
+// Train Name
+// Destination 
+// First Train Time -- in military time
+// Frequency -- in minutes
+// Code this app to calculate when the next train will arrive; this should be relative to the current time.
+// Users from many different machines must be able to view same train times.
+// Styling and theme are completely up to you. Get Creative!
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD68F7-1RrwAQJiT1xSERfEqFNbutwxItc",
+    authDomain: "train-times-82453.firebaseapp.com",
+    databaseURL: "https://train-times-82453.firebaseio.com",
+    projectId: "train-times-82453",
+    storageBucket: "train-times-82453.appspot.com",
+    messagingSenderId: "242923079720"
+  };
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
