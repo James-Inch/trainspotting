e023c96a5eb6d71135a4226a693b74af5dee162b
james inch <james.inch0103@gmail.com>
2018-08-10 19:12:59 -0400

    train values captured

--- assets/apps.js
+++ assets/apps.js
@@ -6,21 +6,48 @@
 // Code this app to calculate when the next train will arrive; this should be relative to the current time.
 // Users from many different machines must be able to view same train times.
 // Styling and theme are completely up to you. Get Creative!
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD68F7-1RrwAQJiT1xSERfEqFNbutwxItc",
-    authDomain: "train-times-82453.firebaseapp.com",
-    databaseURL: "https://train-times-82453.firebaseio.com",
-    projectId: "train-times-82453",
-    storageBucket: "train-times-82453.appspot.com",
-    messagingSenderId: "242923079720"
-  };
-  firebase.initializeApp(config);
-  
-  var database = firebase.database();
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD68F7-1RrwAQJiT1xSERfEqFNbutwxItc",
+  authDomain: "train-times-82453.firebaseapp.com",
+  databaseURL: "https://train-times-82453.firebaseio.com",
+  projectId: "train-times-82453",
+  storageBucket: "train-times-82453.appspot.com",
+  messagingSenderId: "242923079720"
+};
+firebase.initializeApp(config);
 
-  // I need the math thing for sure
-  // train name, destination, and frequency will be constants
-  //submit button will need s click function where val()'s will be sent to the database and also display them in the html 
-  // variables for everything 
-  
+var database = firebase.database();
+
+$("button").on("click", function (event) {
+  event.preventDefault();
+
+  // make variables to hold user inputs
+  var trainName = $("#trainName").val().trim();
+  var destiny = $("#destination").val().trim();
+  var firstTrain = $("#firstTrain").val().trim();
+  var freq = $("#frequency").val().trim();
+
+  // Push train values(user inputs) to database
+  database.ref().push(trainName);
+  database.ref().push(destiny);
+  database.ref().push(firstTrain);
+  database.ref().push(freq);
+
+  // Console.log train values 
+  console.log(trainName);
+  console.log(destiny);
+  console.log(firstTrain);
+  console.log(freq);
+
+  // clear values of train inputs 
+  $("#trainName").val("");
+  $("#destination").val("");
+  $("#firstTrain").val("");
+  $("#frequency").val("");
+});
+
+// I'll need some moments js action for when the next train will arrive
+// train name, destination, and frequency will be constants
+//submit button will need s click function where val()'s will be sent to the database and also display them in the html 
+// variables for everything 
